a5261d632cb1f64f999d8202c2a918619d4cfe3a
Matheus Fritzke <matheusfritzkepaypal@gmail.com>
2024-03-31 23:25:33 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './global.css'
+
+import {RouterProvider} from 'react-router-dom'
+import { router } from './routes'
+export function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
